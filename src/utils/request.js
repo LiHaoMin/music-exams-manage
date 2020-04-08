c9ccc108299457c5533982a9703b7243fdeb2a96
lihaomin <lihaomin666@qq.com>
2020-04-08 17:18:53 +0800

    token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -95,6 +95,17 @@ service.interceptors.response.use(
           duration: 5 * 1000
         })
         break
+      case 401:
+        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+          confirmButtonText: 'Re-Login',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+        break
     }
 
     return Promise.reject(error)
