60b7e47f7f958a7e2f5384c93849d9b39709465d
lihaomin <lihaomin666@qq.com>
2020-04-08 17:17:54 +0800

    token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -57,9 +57,9 @@ service.interceptors.response.use(
 
       // token失效
       if (res.code === 10004) {
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('登录信息已失效，点击确认重新登录。', {
+          confirmButtonText: '确认',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
