bbd2adcff4f46efcdd63adb928463980611daa81
lihaomin <lihaomin666@qq.com>
2020-04-17 11:35:36 +0800

    prod

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,8 +74,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin1',
-        password: '123456'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
